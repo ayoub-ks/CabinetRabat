cc89ef00f1368a603728b60268ba58b5f763586d
Mohamed <mohamedbendahui7@gmail.com>
2020-04-08 17:37:22 +0100

    NavBar update background reservation

--- src/main/java/com/web/app/entity/Period.java
+++ src/main/java/com/web/app/entity/Period.java
@@ -1,10 +1,10 @@
 package com.web.app.entity;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Period {
